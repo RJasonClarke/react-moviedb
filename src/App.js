713b4a00ba1f78482fbe4002c2d1afdcc2cdf93b
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-06-01 10:56:03 -0500

    added api info for later

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import "./App.css"
 import Header from "./components/Header";
 
+// https://api.themoviedb.org/3/movie/popular?api_key=1f441d16d9e32be5307f53437c47ba03&
+
 class App extends Component{
 
   render(){
